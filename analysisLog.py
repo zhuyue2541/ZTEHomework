78ecf04a89565fa746b4eebb51508dac799f5e89
zhuyue <zhuyue2541@outlook.com>
2017-07-17 12:10:25 +0800

    python realize get commit

--- analysisLog.py
+++ analysisLog.py
@@ -11,8 +11,13 @@ for a in au:
 #email	
 email=os.popen('''git log -3 --pretty="%ae" ''')
 em=email.read().splitlines()	
-f.write("email is:\n")
+f.write("\nemail is:\n")
 for a in em:
 	f.write(a+"\n")	
-	
+#commit
+commit=os.popen('''git log -3 --pretty="%H" ''')	
+co=commit.read().splitlines()
+f.write("\ncommit is:\n")
+for a in co:
+	f.write(a+"\n")	
 f.close()
