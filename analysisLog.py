b4d2332d25d24043c3d1418f9317a3f82b975e49
zhuyue <zhuyue2541@outlook.com>
2017-07-17 12:38:00 +0800

    add try in python file

--- analysisLog.py
+++ analysisLog.py
@@ -1,35 +1,54 @@
 import os
-au=[]
-#name
-author=os.popen('''git log -3 --pretty="%an" ''')
-au=author.read().splitlines()
-print au
 f=file("log.txt","w")
-f.write("author name is:\n")
-for a in au:
-	f.write(a+"\n")
+#name
+try:
+	author=os.popen('''git log -3 --pretty="%an" ''')
+	au=author.read().splitlines()
+	print au
+	f.write("author name is:\n")
+	for a in au:
+		f.write(a+"\n")
+except:
+		print "can not get name"
 #email	
-email=os.popen('''git log -3 --pretty="%ae" ''')
-em=email.read().splitlines()	
-f.write("\nemail is:\n")
-for a in em:
-	f.write(a+"\n")	
+try:
+	email=os.popen('''git log -3 --pretty="%ae" ''')
+	em=email.read().splitlines()
+	print em
+	f.write("\nemail is:\n")
+	for a in em:
+		f.write(a+"\n")	
+except:
+		print "can not get email"		
 #commit
-commit=os.popen('''git log -3 --pretty="%H" ''')	
-co=commit.read().splitlines()
-f.write("\ncommit is:\n")
-for a in co:
-	f.write(a+"\n")	
+try:
+	commit=os.popen('''git log -3 --pretty="%H" ''')	
+	co=commit.read().splitlines()
+	print co
+	f.write("\ncommit is:\n")
+	for a in co:
+		f.write(a+"\n")	
+except:
+		print "can not get commit number"			
 #time
-changeTime=os.popen('''git log -3 --pretty="%ad" ''')
-ti=changeTime.read().splitlines()
-f.write("\nchange time is:\n")
-for a in ti:
-	f.write(a+'\n')	
+try:
+	changeTime=os.popen('''git log -3 --pretty="%ad" ''')
+	ti=changeTime.read().splitlines()
+	print ti
+	f.write("\nchange time is:\n")
+	for a in ti:
+		f.write(a+'\n')	
+except:
+		print "can not get time"		
 #description
-description=os.popen('''git log -3 --pretty="%s" ''')
-de=description.read().splitlines()
-f.write("\ndescription is:\n")
-for a in de:
-	f.write(a+'\n')	
-f.close()
+try:
+	description=os.popen('''git log -3 --pretty="%s" ''')
+	de=description.read().splitlines()
+	print de
+	f.write("\ndescription is:\n")
+	for a in de:
+		f.write(a+'\n')	
+except:
+		print "can not get description"	
+finally:		
+	f.close()
